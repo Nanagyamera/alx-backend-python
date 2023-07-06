a322fc50aa55c7cb0358f6cd34d5021db2fb4e23
Nanagyamera <nanagyamera3@gmail.com>
2023-07-06 07:45:23 -0700

    Add type-annotated function for creating a multiplier function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiplier_function(number: float) -> float:
+        return number * multiplier
+    return multiplier_function
