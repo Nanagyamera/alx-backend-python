5386b8326083cadcb47b26db0abb7d84432448fd
Nanagyamera <nanagyamera3@gmail.com>
2023-07-06 07:46:52 -0700

    Add type annotations for safely_get_value function

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from typing import Dict, Any
+
+def safely_get_value(dct: Dict[Any, Any], key: Any, default: Any = None) -> Any:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
