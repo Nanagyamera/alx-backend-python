153bc98a99cb512c69e932e6b09c87e1a1e422e6
Nanagyamera <nanagyamera3@gmail.com>
2023-07-06 14:07:04 -0700

    Add duck-typed annotations for safe_first_element function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+"""
+task 10's module
+"""
 from typing import Optional, Any
 
-
 def safe_first_element(lst: Optional[list]) -> Any:
+    """
+    correct duck-typed annotations
+    """
     if lst:
         return lst[0]
     else:
