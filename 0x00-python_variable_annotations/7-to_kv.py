78c003b7ee5eaff2010ed69dc5fa7137085d9a4a
Nanagyamera <nanagyamera3@gmail.com>
2023-07-06 14:02:01 -0700

    Add type-annotated function for creating a tuple with string key and squared value

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+task 7's module
+"""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    returns a tuple.
+    """
     return k, float(v) ** 2
