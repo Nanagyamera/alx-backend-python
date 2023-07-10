d955b582f0b038c0095ef15375deb010ea961e27
Nanagyamera <nanagyamera3@gmail.com>
2023-07-10 08:50:37 -0700

    Added an asynchronous coroutine 'wait_random' to introduce random delays

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,9 +6,9 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int=10):
     """
-    waits for a random number of seconds
+    waits for a random delay between 0 and max_delay
     """
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
