f6cfb5eb1a1ce456470a71340c02ad0aea971eba
Nanagyamera <nanagyamera3@gmail.com>
2023-07-10 14:36:03 -0700

    Added the 'measure_time' function to calculate the average execution time for 'wait_n'.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 """
-task 3's module
+task 2's module
 """
+import asyncio
 import time
-from concurrent_execution import wait_n  # Importing wait_n from the previous file
 
 
-def measure_time(n, max_delay):
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
     """
-    the average runtime of wait_n
+    Computes the average runtime of wait_n.
     """
     start_time = time.time()
-
-    delays = wait_n(n, max_delay)
-
-    end_time = time.time()
-    total_time = end_time - start_time
-
-    return total_time / n
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
