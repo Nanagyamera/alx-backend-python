0ab0c2ea2d0b3c65743d40e85aa36b639b98b8b9
Nanagyamera <nanagyamera3@gmail.com>
2023-07-10 14:23:27 -0700

    Implemented the async routine 'wait_n' to spawn 'wait_random' coroutines with specified delays and return delays in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,15 +3,17 @@
 Task 1's module
 """
 import asyncio
-from random_delay import wait_random  # Importing wait_random from the previous file
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Executes wait_random n times.
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    completed_tasks, _ = await asyncio.wait(tasks)
-    delays = [task.result() for task in completed_tasks]
-    delays.sort()  # Sort the list of delays in ascending order
-    return delays
+    delays = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(delays)
