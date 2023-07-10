840e2723d22ecfe9459d5ecfb006caf2bdcf60ef
Nanagyamera <nanagyamera3@gmail.com>
2023-07-10 09:17:25 -0700

    new async function 'task_wait_n' which uses 'task_wait_random' to spawn concurrent tasks and returns delays in ascending order

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,14 +3,12 @@
 Task 4's module
 """
 import asyncio
-from basic_async_syntax import wait_random, task_wait_random  # Importing wait_random and task_wait_random from the previous file
+from typing import List
+from previous_file import task_wait_random  # Importing task_wait_random from the previous file
 
-async def task_wait_n(n, max_delay):
-    """
-    computes task_wait_random n times
-    """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    completed_tasks, _ = await asyncio.wait(tasks)
-    delays = [task.result() for task in completed_tasks]
-    delays.sort()  # Sort the list of delays in ascending order
-    return delays
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
