ee328d020a58c1cce134ba6d1d652eadc9c51402
Nanagyamera <nanagyamera3@gmail.com>
2023-07-11 14:31:49 -0700

    Added coroutine async_generator that yields random numbers asynchronously

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,4 +12,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
-        yield random.randint(0, 10)  # Yield a random number between 0 and 10
+        yield random.random() * 10
