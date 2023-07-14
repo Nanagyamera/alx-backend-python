5b664bf6502c082896adf1e8b2a0191f63491e79
Nanagyamera <nanagyamera3@gmail.com>
2023-07-14 08:23:18 -0700

    Added coroutine async_comprehension that collects 10 random numbers using an async comprehension over async_generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,9 @@ import asyncio
 import random
 
 async def async_generator() -> float:
+    """
+    Collects and returns 10 random numbers
+    """
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
         yield random.uniform(0, 10)  # Yield a random float between 0 and 10
